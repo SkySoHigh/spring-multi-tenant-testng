81c0f3be0227e93d0a7a06801a7a2d1f67cf4557
SkySoHigh <skysohigh39@gmail.com>
2022-11-30 14:07:53 +0300

    Added ResourceLoader

---
+++ app/src/main/java/spring/multi/tenant/test/core/loader/ResourceLoader.java
@@ -0,0 +1,26 @@
+package spring.multi.tenant.test.core.loader;
+
+import javax.validation.constraints.NotNull;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+/**
+ * The ResourceParser class provides access to methods for reading files found in classpath (resource folder)
+ */
+public class ResourceLoader {
+
+    /**
+     * Reads file from the resource folder
+     *
+     * @param fileName Name of the file
+     * @return InputStream
+     * @throws FileNotFoundException if there is no such file in the classpath
+     */
+    public static @NotNull InputStream readFile(String fileName) throws FileNotFoundException {
+        InputStream stream = ResourceLoader.class.getClassLoader().getResourceAsStream(fileName);
+        if (stream == null) {
+            throw new FileNotFoundException(fileName + " file not found in the classpath");
+        }
+        return stream;
+    }
+}
